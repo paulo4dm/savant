fb2a8ee574c45218916b7545623d3dff96ed555e
Paulo Mesquita <paulo.adm@gmail.com>
2017-03-09 18:17:13 -0400

    Autenticação

--- savant/social/views.py
+++ savant/social/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login(request):
+    return render(request, 'registration/login.html', {})
+
+@login_required
 def index(request):
     return render(request, 'social/index.html', {})
