83124dee1cc7f428bccaa7b07a3d419f6cf3642b
Paulo Mesquita <paulo.adm@gmail.com>
2017-03-16 15:52:47 -0400

    Areas de Interesse

--- savant/social/urls.py
+++ savant/social/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^profile$', views.profile, name='profile'),
+    url(r'^matematica$', views.math_list, name='matematica'),
+    url(r'^informatica$', views.info_list, name='informatica'),
 ]
--- savant/social/views.py
+++ savant/social/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -8,3 +9,17 @@ def login(request):
 @login_required
 def index(request):
     return render(request, 'social/index.html', {})
+
+@login_required
+def profile(request):
+    return render(request, 'social/profile.html', {})
+
+@login_required
+def math_list(request):
+    users = User.objects.filter(groups__name='matematica')
+    return render(request, 'social/matematica.html', {'users': users})
+
+@login_required
+def info_list(request):
+    users = User.objects.filter(groups__name='informatica')
+    return render(request, 'social/informatica.html', {'users': users})
